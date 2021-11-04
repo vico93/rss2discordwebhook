ae9306a7ccdbf32f35d38077476693f9968c35fb
Vico <vpontin93@gmail.com>
2021-11-04 13:01:58 -0300

    Update script.sh

--- src/script.sh
+++ src/script.sh
@@ -4,10 +4,8 @@
 
 while IFS="|" read -r rec_column1 rec_column2
 do
-	echo "Título: $rec_column1"
-	echo "Link: $rec_column2"
-	echo ""
-	./discord.sh --webhook-url=$2 --username $3 --avatar $4 --text "$rec_column1\n🔗 $rec_column2"
+	./discord.sh --webhook-url=$2 --username $3 --avatar $4 --text "**$rec_column1**\n🔗 $rec_column2"
+	echo "[INFO] Novo feed encontrado: $rec_column1 - Link: $rec_column2"
 	# Para evitar rate limit
 	sleep 1
 done < <(rsstail -i $5 --format '%(title)s|%(link)s\n' -u $1  -n 0)
